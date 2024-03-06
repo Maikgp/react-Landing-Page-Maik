20b60ec9abe0642114bf53348dfe226486e03c20
Maikel Guerra Pantoja <154547587+Maikgp@users.noreply.github.com>
2024-03-06 20:31:02 +0000

    feat: proyect ready

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,15 +5,17 @@ import React from "react";
 //create your first component
 const Home = (num) => {
 	return (
-		<div id="mainBox">
-			<div className="box"><i className={"fa fa-clock"}></i></div>			
-			<div className="box">{num.six}</div>
-			<div className="box">{num.five}</div>
-			<div className="box">{num.four}</div>
-			<div className="box">{num.three}</div>
-			<div className="box">{num.two}</div>
+		<div id="mySquare">
+			<div className="square">
+				<i className={"fa fa-clock"}></i>
+			</div>			
+			<div className="square">{num.six}</div>
+			<div className="square">{num.five}</div>
+			<div className="square">{num.four}</div>
+			<div className="square">{num.three}</div>
+			<div className="square">{num.two}</div>
 			<div>,</div>
-			<div className="box">{num.one}</div>
+			<div className="square">{num.one}</div>
 		</div>
 		
 		
--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// include your styles into the webpack bundle
+
 import "../styles/index.css";
 
-//import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
+
 let counter = 0;
 setInterval(() => {
     ReactDOM.render(<Home 
