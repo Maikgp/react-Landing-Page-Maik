dcb75400abfa0884470d04819c588fba2616be63
Maikel Guerra Pantoja <154547587+Maikgp@users.noreply.github.com>
2024-03-14 21:50:00 +0000

    feat: falta todavia

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,22 +1,43 @@
 import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
-	const [inputValue, setInputValue] = useState("")
-	const [todo,setTodo] = useState([])
-
-
 	return (
-		<div className="text-center">
-			<h1> My To do</h1>
-			
+
+		<div className="container">
+
+			<h1>My Todo</h1>
+			<ul>
+				<li><input type="text" placeholder="what do you need to do?" ></input></li>
+				<li>walk the dog</li>
+				<li>take breasf</li>
+				<li>take a food</li>
+			</ul>
+			<div>23 tasks</div>
 		</div>
-	)
+		       
+	);
 
 
 };
+
+
+
+/* const [inputValue, setInputValue] = useState("")
+const [todo,setTodo] = useState([])
+
+
+return (
+	<div className="text-center">
+		<h1> My To do</h1>
+		
+		
+	</div>
+) */
+
+
+
 
 export default Home;
