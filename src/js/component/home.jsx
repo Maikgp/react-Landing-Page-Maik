1ebe21965d8358285cd5eed13e3dbb3217e7b541
Maikel Guerra Pantoja <154547587+Maikgp@users.noreply.github.com>
2024-03-15 20:05:26 +0000

    feat: listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [inputValue, setInputValue] = useState("");
+    const [todo, setTodo] = useState([]);
+
+    return (
+        <div className="container">
+            <div className="text-center">
+                <h1>My todos</h1>
+                <div>
+                    <ul className="list-group">
+                        <li className="list-group-item">
+                            <input
+                                type="text"
+                                onChange={(e) => setInputValue(e.target.value)}
+                                value={inputValue}
+                                onKeyPress={(e) => {
+                                    if (e.key === "Enter") {
+                                        setTodo(todo.concat([inputValue]));
+                                        setInputValue("");
+                                    }
+                                }}
+                                placeholder="What do you need to do?"
+                            />
+                        </li>
+                        {todo.map((item, index) => (
+                            <li className="list-group-item d-flex justify-content-between" key={index}>
+                                <span className="todo_list_task">
+                                    {item}
+                                </span>
+                                <span className="todoList_delete">
+                                    <i
+                                        className="fa fa-trash"
+                                        onClick={() =>
+                                            setTodo(
+                                                todo.filter((t, currentIndex) => index !== currentIndex)
+                                            )
+                                        }
+                                    ></i>
+                                </span>
+                            </li>
+                        ))}
+                    </ul>
+                    <div id="task" className="white-text">{todo.length} task</div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
