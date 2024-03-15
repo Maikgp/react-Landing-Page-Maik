a79cd773b5abc77b1b280beae33478df1aa25232
Maikel Guerra Pantoja <154547587+Maikgp@users.noreply.github.com>
2024-03-15 20:14:49 +0000

    feat:proyect listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,51 +1,50 @@
 import React, { useState } from "react";
 
-
-
-
 const Home = () => {
-
-	const [inputValue, setInputValue] = useState("")
-	const [todo, setTodo] = useState([])
-
+	const [inputValue, setInputValue] = useState("");
+	const [todo, setTodo] = useState([]);
 
 	return (
-		<div className="text-center">
-			<h1>My todos</h1>
-			<div>
-				<ul className="list-group">
-					<li className="list-group-item">
-						<input
-							type="text"
-							onChange={(e) => setInputValue(e.target.value)}
-							value={inputValue}
-							onKeyPress={(e) => {
-								if (e.key === "Enter") {
-									setTodo(todo.concat([inputValue]));
-									setInputValue("");
-								}
-							}}
-							placeholder="What do you need to do?">
-						</input>
-					</li>
-					{todo.map((item, index) => (
-						<li className="list-group-item d-flex justify-content-between">
-							<span className="todo_list_task">
-								{item}
-							</span>
-							<span className="todoList_delete">
-								<i className="fa fa-trash"
-									onClick={() => setTodo(todo.filter((t, currentIndex) => index != currentIndex))}></i>
-							</span>
-
-						</li>
-					))}
-				</ul>
+		<div className="container">
+			<div className="text-center">
+				<h1>My To Do</h1>
 				<div>
-					{todo.length} task
+					<ul className="list-group">
+						<li className="list-group-item">
+							<input
+								type="text"
+								onChange={(e) => setInputValue(e.target.value)}
+								value={inputValue}
+								onKeyPress={(e) => {
+									if (e.key === "Enter") {
+										setTodo(todo.concat([inputValue]));
+										setInputValue("");
+									}
+								}}
+								placeholder="What do you need to do?"
+							/>
+						</li>
+						{todo.map((item, index) => (
+							<li className="list-group-item d-flex justify-content-between" key={index}>
+								<span className="todo_list_task">
+									{item}
+								</span>
+								<span className="todoList_delete">
+									<i
+										className="fa fa-trash"
+										onClick={() =>
+											setTodo(
+												todo.filter((t, currentIndex) => index !== currentIndex)
+											)
+										}
+									></i>
+								</span>
+							</li>
+						))}
+					</ul>
+					<div id="task" className="white-text">{todo.length} task</div>
 				</div>
 			</div>
-
 		</div>
 	);
 };
