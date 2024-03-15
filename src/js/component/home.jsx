18f6231ca32dee5d0acfed1e9e547269450c6255
Maikel Guerra Pantoja <154547587+Maikgp@users.noreply.github.com>
2024-03-15 20:19:09 +0000

    en proceso

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,6 +4,30 @@ const Home = () => {
     const [inputValue, setInputValue] = useState("");
     const [todo, setTodo] = useState([]);
 
+	fetch('https://playground.4geeks.com/apis/fake/todos/user/alesanchezr', {
+      method: "PUT",
+      body: JSON.stringify(todos),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(resp => {
+        console.log(resp.ok); // Será true si la respuesta es exitosa
+        console.log(resp.status); // El código de estado 200, 300, 400, etc.
+        console.log(resp.text()); // Intentará devolver el resultado exacto como string
+        return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
+    })
+    .then(data => {
+        // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+        console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+    })
+    .catch(error => {
+        // Manejo de errores
+        console.log(error);
+    });
+
+
+
     return (
         <div className="container">
             <div className="text-center">
